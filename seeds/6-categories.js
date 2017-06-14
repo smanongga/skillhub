ecce6b9c6ac642aaf6155e13f9d6fb8add683549
S. Manongga <smanongga@gmail.com>
2017-06-15 11:43:51 +1200

    Added seed files for skills and categories

--- seeds/6-categories.js
+++ seeds/6-categories.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('categories').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, skill_id: 1, name: 'Music'},
+        {id: 2, skill_id: 2, name: 'Web Development'},
+        {id: 3, skill_id: 3, name: 'Art and Design'}
       ])
     })
 }
--- seeds/7-skills.js
+++ seeds/7-skills.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('skills').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, name: 'Guitar'},
+        {id: 2, name: 'Javascript'},
+        {id: 3, name: 'Photography'}
       ])
     })
 }
