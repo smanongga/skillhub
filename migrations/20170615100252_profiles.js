e565a55878cabb2545f43727104a38d81737a5c8
S. Manongga <smanongga@gmail.com>
2017-06-15 10:52:44 +1200

    Fix FK issue when migrating

--- migrations/20170615100252_profiles.js
+++ migrations/20170615100252_profiles.js
@@ -1,5 +1,6 @@
 exports.up = knex => knex.schema.createTable('profiles', table => {
   table.increments('id').primary()
+  table.integer('user_id').unsigned()
   table.foreign('user_id').references('users.id')
   table.string('first_name')
   table.string('last_name')
