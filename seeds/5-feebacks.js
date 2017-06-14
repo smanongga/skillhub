394f4fecc3e58cdd72fdd819a83883ace45b8f95
S. Manongga <smanongga@gmail.com>
2017-06-15 11:33:54 +1200

    Delete categories seed

--- seeds/5-feebacks.js
+++ seeds/5-feebacks.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('feedbacks').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('feedbacks').insert([
         {id: 1, colName: 'rowValue1'},
         {id: 2, colName: 'rowValue2'},
         {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      ])
+    })
+}
---
+++ seeds/6-categories.js
@@ -0,0 +1,13 @@
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('categories').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('categories').insert([
+        {id: 1, colName: 'rowValue1'},
+        {id: 2, colName: 'rowValue2'},
+        {id: 3, colName: 'rowValue3'}
+      ])
+    })
+}
