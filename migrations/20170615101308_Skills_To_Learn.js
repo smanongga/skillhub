4e5e754a866aae1581ae62c97aa3f0505b30ede3
S. Manongga <smanongga@gmail.com>
2017-06-15 10:58:41 +1200

    Fix typo

--- migrations/20170615101308_Skills_To_Learn.js
+++ migrations/20170615101308_Skills_To_Learn.js
@@ -4,7 +4,7 @@ exports.up = knex => knex.schema.createTable('skills_to_learn', table => {
   table.integer('profile_id').unsigned()
   table.foreign('profile_id').references('profiles.id')
   table.integer('skills_id').unsigned()
-  table.foreign('skils_id').references('skills.id')
+  table.foreign('skills_id').references('skills.id')
 })
 
 exports.down = knex => knex.schema.dropTable('skills_to_learn')
