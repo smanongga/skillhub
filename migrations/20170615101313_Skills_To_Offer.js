c0f57fe4d7baba469a2fa6fe0c5c5b9495a8aebd
S. Manongga <smanongga@gmail.com>
2017-06-15 10:56:42 +1200

    Fix issue with FK

--- migrations/20170615101313_Skills_To_Offer.js
+++ migrations/20170615101313_Skills_To_Offer.js
@@ -1,7 +1,9 @@
 
 exports.up = knex => knex.schema.createTable('skills_to_offer', table => {
   table.increments('id').primary()
+  table.integer('profile_id').unsigned()
   table.foreign('profile_id').references('profiles.id')
+  table.integer('skills_id').unsigned()
   table.foreign('skils_id').references('skills.id')
 })
 
