87114b72a3bc25581fa729e8be5d1e3e043d309e
S. Manongga <smanongga@gmail.com>
2017-06-15 10:48:32 +1200

    Forgot to commit skills_to_learn migration file

--- migrations/20170615101308_Skills_To_Learn.js
+++ migrations/20170615101308_Skills_To_Learn.js
@@ -3,6 +3,6 @@ exports.up = knex => knex.schema.createTable('skills_to_learn', table => {
   table.increments('id').primary()
   table.foreign('profile_id').references('profiles.id')
   table.foreign('skils_id').references('skills.id')
-};
+})
 
 exports.down = knex => knex.schema.dropTable('skills_to_learn')
