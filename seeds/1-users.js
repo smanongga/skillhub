5d3d1cc36e42274d9d6e89baf21bf1a9c67f073e
S. Manongga <smanongga@gmail.com>
2017-06-15 13:17:16 +1200

    Fix seed import

--- seeds/1-users.js
+++ seeds/1-users.js
@@ -1,17 +1,17 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, password: 'password123',username: 'samM'},
-        {id: 2, password: 'password123',username: 'rob'},
-        {id: 3, password: 'password123',username: 'rory'},
-        {id: 4, password: 'password123',username: 'julie'},
-        {id: 5, password: 'password123',username: 'samH'},
-        {id: 6, password: 'password123',username: 'tony'},
-        {id: 7, password: 'password123',username: 'don'}
-      ]);
-    });
-};
+        {id: 1, password: null, username: 'samM'},
+        {id: 2, password: null, username: 'rob'},
+        {id: 3, password: null, username: 'rory'},
+        {id: 4, password: null, username: 'julie'},
+        {id: 5, password: null, username: 'samH'},
+        {id: 6, password: null, username: 'tony'},
+        {id: 7, password: null, username: 'don'}
+      ])
+    })
+}
--- seeds/3-skills-to-learn.js
+++ seeds/3-skills-to-learn.js
@@ -4,16 +4,16 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('skills_to_learn').insert([
-        {id: 1, profile_id: 2, skill_id: 2},
-        {id: 2, profile_id: 2, skill_id: 4},
-        {id: 3, profile_id: 6, skill_id: 5},
-        {id: 4, profile_id: 4, skill_id: 3},
-        {id: 5, profile_id: 3, skill_id: 2},
-        {id: 6, profile_id: 2, skill_id: 2},
-        {id: 7, profile_id: 6, skill_id: 5},
-        {id: 8, profile_id: 6, skill_id: 6},
-        {id: 9, profile_id: 6, skill_id: 3},
-        {id: 10, profile_id: 7, skill_id: 1}
-      ]);
-    });
-};
+        {id: 1, profile_id: 2, skills_id: 2},
+        {id: 2, profile_id: 2, skills_id: 4},
+        {id: 3, profile_id: 6, skills_id: 5},
+        {id: 4, profile_id: 4, skills_id: 3},
+        {id: 5, profile_id: 3, skills_id: 2},
+        {id: 6, profile_id: 2, skills_id: 2},
+        {id: 7, profile_id: 6, skills_id: 5},
+        {id: 8, profile_id: 6, skills_id: 6},
+        {id: 9, profile_id: 6, skills_id: 3},
+        {id: 10, profile_id: 7, skills_id: 1}
+      ])
+    })
+}
