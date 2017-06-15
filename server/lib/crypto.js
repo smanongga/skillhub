5cbda90c4cd4e7ecd0c2ef04f5e407227e29311f
eda <julie.crutchley@outlook.com>
2017-06-15 18:34:24 +1200

    add sodium refs back in 2 crypto files and package.json

--- server/lib/crypto.js
+++ server/lib/crypto.js
@@ -1,20 +1,20 @@
-// const sodium = require('sodium').api
+const sodium = require('sodium').api
 
-// module.exports = {
-//   getHash,
-//   verifyUser
-// }
+module.exports = {
+  getHash,
+  verifyUser
+}
 
-// function getHash (password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str(
-//     passwordBuffer,
-//     sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-//     sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
-//   )
-// }
+function getHash (password) {
+  const passwordBuffer = Buffer.from(password, 'utf8')
+  return sodium.crypto_pwhash_str(
+    passwordBuffer,
+    sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+    sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+  )
+}
 
-// function verifyUser (user, password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str_verify(user.hash, passwordBuffer)
-// }
+function verifyUser (user, password) {
+  const passwordBuffer = Buffer.from(password, 'utf8')
+  return sodium.crypto_pwhash_str_verify(user.hash, passwordBuffer)
+}
