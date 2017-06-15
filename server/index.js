fd62e7aac8d3a0c1b6cfb079b29c75ae703d408b
S. Manongga <smanongga@gmail.com>
2017-06-15 13:35:14 +1200

    Added db connection in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 /* eslint no-console: "off" */
 require('dotenv').config()
 
-const app = require('./server')
-const port = process.env.PORT || 3000
+const createServer = require('./server')
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[environment]
+const connection = require('knex')(config)
+const server = createServer(connection)
 
-app.listen(port, () => {
-  console.log('Server listening on port', port)
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, function () {
+  console.log('Listening on port', PORT)
 })
