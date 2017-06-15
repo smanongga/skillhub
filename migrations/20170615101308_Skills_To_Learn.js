79cc0753e90b74dd3d3db019a8653690eadfbcb9
eda <julie.crutchley@outlook.com>
2017-06-15 18:13:47 +1200

    alter migrations

--- migrations/20170615101308_Skills_To_Learn.js
+++ migrations/20170615101308_Skills_To_Learn.js
@@ -4,7 +4,6 @@ exports.up = knex => knex.schema.createTable('skills_to_learn', table => {
   table.integer('profile_id').unsigned()
   table.foreign('profile_id').references('profiles.id')
   table.integer('skills_id').unsigned()
-  table.foreign('skills_id').references('skills.id')
 })
 
 exports.down = knex => knex.schema.dropTable('skills_to_learn')
