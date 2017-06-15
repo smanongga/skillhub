d135823334e3add14aebebb18869eb623e82c5bf
Rob <superserva@gmail.com>
2017-06-15 19:27:27 +1200

    Added router get profile by id

--- server/routes/api.js
+++ server/routes/api.js
@@ -69,4 +69,55 @@ router.get('/secret', (req, res) => {
   })
 })
 
+router.get('/profile/:id', (req, res) => {
+  const connection = req.app.get('db')
+  db.getProfileById(Number(req.params.id), connection)
+  .then((data) => {
+    res.json({result: data})
+  })
+})
+// Expecting this type of data back:
+// { id: 1,
+//  name: tony
+//  photo_url: 'www.Tonyphoto.com'
+//  bio: “Hi, I'm Tony. Looking forward to learning with you”
+//    skillsToOffer['car repair', 'baking']
+//    skillsToLearn['guitar','javascript']
+//    feedback[{
+//               commenter_username: 'Jim'
+//               commenter_photo_url: 'www.photo/132.png'
+//               comment: 'Tony was great at teaching my how to bake a cake!'
+//            }]
+// }
+
+router.get('/categories', (req, res) => {
+  const connection = req.app.get('db')
+  db.getCategories(connection)
+  .then((data) => {
+    res.json({result: data})
+  })
+})
+// Expecting this type of data back:
+// [
+//    { id: 1, name: 'Music'}
+//    { id: 2, name: 'Web Development'}
+//    { id: 3, name: 'Art and Design'}
+// ]
+
+
+// GET /pofiles/skills/:name
+// Needs to return profile object with array of skills:
+// { id: 1,
+//  name: tony
+//    skillsToLearn['guitar','javascript']
+// }
+
+
+// GET /pofiles/skills/:name
+// Needs to return profile object with array of skills:
+// { id: 1,
+//  name: tony
+//    skillsToLearn['guitar','javascript']
+// }
+
 module.exports = router
