2be2e637fa86a60e33d46c9c454672903b22e899
keewhy <samhoulahan625@gmail.com>
2017-06-18 15:49:30 +1200

    added more auth functionality

--- client/actions/logout.js
+++ client/actions/logout.js
@@ -1,17 +1,22 @@
 
+import AuthService from '../utils/auth0'
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
+
+const authService = new AuthService('elBcVpwtrkufH2NWvkGQAzW1XRigLLbK',
+  'meal-mate.au.auth0.com')
 
 function logoutSuccess (profile) {
   return {
     type: LOGOUT_SUCCESS
   }
 }
-export function logout () {
+
+export function logout (history) {
   return dispatch => {
-    localStorage.removeItem('id_token')
-    localStorage.removeItem('user')
+    console.log(history)
+    authService.logout()
     return dispatch(logoutSuccess())
   }
 }
