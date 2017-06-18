e6d94fd86231378de59d902d023942787daac1ab
S. Manongga <smanongga@gmail.com>
2017-06-18 20:20:12 +1200

    Added more categories to seed

--- seeds/6-categories.js
+++ seeds/6-categories.js
@@ -7,7 +7,16 @@ exports.seed = function (knex, Promise) {
       return knex('categories').insert([
         {id: 1, name: 'Music'},
         {id: 2, name: 'Tech'},
-        {id: 3, name: 'Arts'}
+        {id: 3, name: 'Arts'},
+        {id: 4, name: 'Language'},
+        {id: 5, name: 'Science'},
+        {id: 6, name: 'Business'},
+        {id: 7, name: 'Writing'},
+        {id: 8, name: 'Culture'},
+        {id: 9, name: 'Culinary'},
+        {id: 10, name: 'Craft'},
+        {id: 11, name: 'Career'},
+        {id: 12, name: 'Sports'}
       ])
     })
 }
