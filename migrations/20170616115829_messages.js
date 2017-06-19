15ff46feeed0916523225d49457aea2804a0b7b4
Julie Crutchley <dan-julz@hotmail.com>
2017-06-19 18:30:05 +1200

    amend messages seed which refers to profiles.id twice as foreign key constraint

--- migrations/20170616115829_messages.js
+++ migrations/20170616115829_messages.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable('messages', table => {
   table.integer('profile_id').unsigned()
   table.foreign('profile_id').references('profiles.id')
   table.integer('sender_id').unsigned()
-  table.foreign('sender_id').references('profiles.id')
+  // table.foreign('sender_id').references('profiles.id')
   table.text('message')
   table.string('subject')
   table.text('time')
