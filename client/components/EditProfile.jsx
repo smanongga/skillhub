5938729f20fc9224e48d173dc3decf89c8bd37db
S. Manongga <smanongga@gmail.com>
2017-06-20 15:51:01 +1200

    Re-arrange import item and take out commented lines

--- client/components/EditProfile.jsx
+++ client/components/EditProfile.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Dropzone from 'react-dropzone'
+import {Typeahead} from 'react-bootstrap-typeahead'
+
 import {updateProfile, addProfileToDb, getLocations, getSkills, addProfileSkillsOffered, addProfileSkillsWanted} from '../actions'
 import {getUsersProfile} from '../actions/index'
 import {uploadImage} from '../utils/api'
-import {Typeahead} from 'react-bootstrap-typeahead'
 
 class EditProfile extends React.Component {
   componentDidMount () {
@@ -169,10 +170,6 @@ class EditProfile extends React.Component {
                     onDrop={this.handleImageDrop}>
                     <p>Drop an image or click to select a file to upload.</p>
                   </Dropzone>}
-                {/* {this.state.profilePic === '' ? null :
-          <div>
-            <img src={this.props.profile.photoUrl} />
-          </div>} */}
                   {this.state.profilePic &&
                     <div>
                       <h4>Upload Successful</h4>
