bc80c5ea8c4adb868548e842fe2602a37b449a6e
keewhy <samhoulahan625@gmail.com>
2017-06-22 09:25:18 +1200

    couple of fixes

--- client/index.js
+++ client/index.js
@@ -7,10 +7,10 @@ import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(thunk)
 ))
 
 document.addEventListener('DOMContentLoaded', () => {
